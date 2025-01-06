3ae847d414cf397aa590f1f0f98d9fe18ed8e426
王林林2_0 <13039364+wang-linlin-2-0@user.noreply.gitee.com>
2025-01-06 11:14:24 +0800

    修改了打印日志

--- lib/my_print.dart
+++ lib/my_print.dart
@@ -1,86 +1,25 @@
-// lib/print_helper.dart
+class Log {
+  static final Log _instance = Log._internal(); // 单例实例
 
-class MyPrint {
-  // 私有静态变量存储唯一实例1
-  static final MyPrint _instance = MyPrint._internal();
+  static bool _isEnabled = true; // 是否启用日志
 
-  // 工厂构造函数返回实例
-  factory MyPrint() => _instance;
+  /// 私有构造函数
+  Log._internal();
 
-  // 私有构造函数
-  MyPrint._internal();
-
-  // 默认为开启日志
-  bool _isLoggingEnabled = true;
-
-  /// 初始化方法，可以进行日志相关的配置
-  void init({bool isLoggingEnabled = true}) {
-    _isLoggingEnabled = isLoggingEnabled;
+  /// 提供一个工厂构造函数，返回同一个实例
+  factory Log() {
+    return _instance;
   }
 
-  /// 设置日志开关
-  void setLoggingEnabled(bool isEnabled) {
-    _isLoggingEnabled = isEnabled;
+  /// 初始化日志工具（可以控制是否打印日志）
+  static void init({bool isEnabled = true}) {
+    _isEnabled = isEnabled;
   }
 
-  /// 打印普通日志
-  void log(String message) {
-    if (_isLoggingEnabled) {
-      print("MYPrint(0.0): $message");
+  /// 打印日志（只需调用 Log.print("message")）
+  static void d(String message) {
+    if (_isEnabled) {
+      print("[MyLog][DEBUG] $message");
     }
-  }
-
-  /// 打印调试日志，只有在调试模式下才显示
-  void debug(String message) {
-    if (_isLoggingEnabled) {
-      assert(() {
-        print("DEBUG: $message");
-        return true;
-      }());
-    }
-  }
-
-  /// 打印警告日志
-  void warn(String message) {
-    if (_isLoggingEnabled) {
-      print("WARNING: $message");
-    }
-  }
-
-  /// 打印错误日志
-  void error(String message) {
-    if (_isLoggingEnabled) {
-      print("ERROR: $message");
-    }
-  }
-
-  /// 打印带时间戳的日志
-  void logWithTime(String message) {
-    if (_isLoggingEnabled) {
-      final timeStamp = DateTime.now().toIso8601String();
-      print("[$timeStamp] LOG: $message");
-    }
-  }
-
-  /// 打印带颜色的日志
-  void logWithColor(String message, {String color = 'green'}) {
-    if (_isLoggingEnabled) {
-      String coloredMessage = _getColoredMessage(message, color);
-      print(coloredMessage);
-    }
-  }
-
-  // 私有方法，用于根据颜色参数返回带颜色的日志
-  String _getColoredMessage(String message, String color) {
-    const colorCodes = {
-      'green': '\x1B[32m',
-      'yellow': '\x1B[33m',
-      'red': '\x1B[31m',
-      'blue': '\x1B[34m',
-      'reset': '\x1B[0m'
-    };
-
-    final colorCode = colorCodes[color] ?? colorCodes['reset'];
-    return '$colorCode$message\x1B[0m';
   }
 }
--- test/my_tool_kit_test.dart
+++ test/my_tool_kit_test.dart
@@ -2,5 +2,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:my_tool_kit/my_print.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  test('adds one to input values', () {
+    Log.init(isEnabled: true);
+
+    // 使用日志
+    Log.d("This is a debug message");
+  });
 }
