456e69a7dbc7d785ba03ba8ab22129e60b334756
王林林2_0 <13039364+wang-linlin-2-0@user.noreply.gitee.com>
2025-02-26 14:41:37 +0800

    修改了蓝牙

--- lib/my_bluetooth.dart
+++ lib/my_bluetooth.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class BluetoothService extends GetxService {
-  // 通过 Get.find() 获取单例实例
+  // 单例获取
   static BluetoothService get to => Get.find<BluetoothService>();
 
   /// 监听蓝牙是否开启
@@ -48,16 +51,47 @@ class BluetoothService extends GetxService {
 
   /// 监听蓝牙状态变化
   void _listenToBluetoothState() {
-    stateSubscription ??=
-        FlutterBluePlus.adapterState.listen((BluetoothAdapterState state) {
+    stateSubscription ??= FlutterBluePlus.adapterState
+        .listen((BluetoothAdapterState state) async {
       if (state == BluetoothAdapterState.on) {
+        // 蓝牙开启时
         isBluetoothOn.value = true;
-        // usually start scanning, connecting, etc
       } else {
+        // 蓝牙关闭时
         isBluetoothOn.value = false;
-        // show an error to the user, etc
+        if (!kIsWeb && Platform.isAndroid) {
+          await _turnOnBluetooth();
+        }
       }
     });
+  }
+
+  /// 尝试开启蓝牙（仅限 Android）
+  Future<void> _turnOnBluetooth() async {
+    if (await Permission.bluetooth.request().isGranted) {
+      await FlutterBluePlus.turnOn();
+    }
+  }
+
+  /// 开始扫描设备
+  Future<void> startScanning() async {
+    if (isBluetoothOn.value && !isScanning.value) {
+      isScanning.value = true;
+      scanResultsSubscription ??= FlutterBluePlus.scanResults.listen((results) {
+        scanResults.value = results;
+      });
+      FlutterBluePlus.startScan(timeout: const Duration(seconds: 4));
+    }
+  }
+
+  /// 停止扫描
+  void stopScanning() {
+    if (isScanning.value) {
+      isScanning.value = false;
+      FlutterBluePlus.stopScan();
+      scanResultsSubscription?.cancel();
+      scanResultsSubscription = null;
+    }
   }
 
   @override
